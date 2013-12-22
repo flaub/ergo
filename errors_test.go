343fe684e00ce5443b580d1d1493f0aafe780e25
Frank Laub <frank.laub@gmail.com>
2013-12-22 01:07:06 -0800

    Only use gocheck, use github mirror

--- errors_test.go
+++ errors_test.go
@@ -24,16 +24,20 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 package ergo
 
 import (
-	"github.com/remogatto/prettytest"
+	gc "github.com/motain/gocheck"
 	"io"
-	gc "launchpad.net/gocheck"
 	"strings"
 	"testing"
 )
 
+func Test(t *testing.T) { gc.TestingT(t) }
+
 type TestSuite struct {
-	prettytest.Suite
 }
+
+var (
+	_ = gc.Suite(new(TestSuite))
+)
 
 const (
 	EMyError0 = ErrCode(iota)
@@ -49,117 +53,111 @@ var (
 	}
 )
 
-func TestRunner(t *testing.T) {
-	prettytest.Run(t,
-		new(TestSuite),
-	)
-}
-
 func NewError(code ErrCode, args ...interface{}) *Error {
 	return New(1, "ergo", code, args...)
 }
 
-func (t *TestSuite) BeforeAll() {
+func (t *TestSuite) SetUpSuite(c *gc.C) {
 	Domain("ergo", errors)
 }
 
-func (t *TestSuite) TestNew() {
+func (t *TestSuite) TestNew(c *gc.C) {
 	err := New(0, "ergo", EMyError0)
-	t.Not(t.Nil(err))
-	t.Equal("ergo", err.Domain)
-	t.Equal(EMyError0, err.Code)
+	c.Check(err, gc.NotNil)
+	c.Check(err.Domain, gc.Equals, "ergo")
+	c.Check(err.Code, gc.Equals, EMyError0)
 	first := strings.SplitN(err.Context, "\n", 3)
-	t.Check(first[1], gc.Matches, "*TestNew$")
-	t.Equal(errors[EMyError0], err.Message())
+	c.Check(first[1], gc.Matches, "*TestNew$")
+	c.Check(err.Message(), gc.Equals, errors[EMyError0])
 	lines := strings.Split(err.Error(), "\n")
-	t.Equal("[ergo:0] My error 0", lines[0])
+	c.Check(lines[0], gc.Equals, "[ergo:0] My error 0")
 }
 
-func (t *TestSuite) TestCustom() {
+func (t *TestSuite) TestCustom(c *gc.C) {
 	err := NewError(EMyError1, "x", 1)
-	t.Not(t.Nil(err))
-	t.Equal("ergo", err.Domain)
-	t.Equal(EMyError1, err.Code)
+	c.Check(err, gc.NotNil)
+	c.Check(err.Domain, gc.Equals, "ergo")
+	c.Check(err.Code, gc.Equals, EMyError1)
 	first := strings.SplitN(err.Context, "\n", 3)
-	t.Check(first[1], gc.Matches, "*TestCustom$")
-	t.Equal(errors[EMyError1], err.Message())
+	c.Check(first[1], gc.Matches, "*TestCustom$")
+	c.Check(err.Message(), gc.Equals, errors[EMyError1])
 	lines := strings.Split(err.Error(), "\n")
-	t.Equal("[ergo:1] My error 1", lines[0])
+	c.Check(lines[0], gc.Equals, "[ergo:1] My error 1")
 
 	err = NewError(EMyErrorArgs, "name", "x")
-	t.Not(t.Nil(err))
-	t.Equal("ergo", err.Domain)
-	t.Equal(EMyErrorArgs, err.Code)
+	c.Check(err, gc.NotNil)
+	c.Check(err.Domain, gc.Equals, "ergo")
+	c.Check(err.Code, gc.Equals, EMyErrorArgs)
 	first = strings.SplitN(err.Context, "\n", 3)
-	t.Check(first[1], gc.Matches, "*TestCustom$")
-	t.Equal("The x failed", err.Message())
+	c.Check(first[1], gc.Matches, "*TestCustom$")
+	c.Check(err.Message(), gc.Equals, "The x failed")
 	lines = strings.Split(err.Error(), "\n")
-	t.Equal("[ergo:2] The x failed", lines[0])
+	c.Check(lines[0], gc.Equals, "[ergo:2] The x failed")
 }
 
-func (t *TestSuite) TestWrap() {
+func (t *TestSuite) TestWrap(c *gc.C) {
 	err := Wrap(io.EOF)
-	t.Not(t.Nil(err))
-	t.Equal("go", err.Domain)
-	t.Equal(ErrCode(0), err.Code)
+	c.Check(err, gc.NotNil)
+	c.Check(err.Domain, gc.Equals, "go")
+	c.Check(err.Code, gc.Equals, ErrCode(0))
 	first := strings.SplitN(err.Context, "\n", 3)
-	t.Check(first[1], gc.Matches, "*TestWrap$")
-	t.Equal("EOF", err.Info["_err"])
-	t.Equal("Error: EOF", err.Message())
+	c.Check(first[1], gc.Matches, "*TestWrap$")
+	c.Check(err.Info["_err"], gc.Equals, "EOF")
+	c.Check(err.Message(), gc.Equals, "Error: EOF")
 	lines := strings.Split(err.Error(), "\n")
-	t.Equal("[go:0] Error: EOF", lines[0])
+	c.Check(lines[0], gc.Equals, "[go:0] Error: EOF")
 
 	err = Wrap("Random error")
-	t.Equal("go", err.Domain)
-	t.Equal(ErrCode(0), err.Code)
+	c.Check(err.Domain, gc.Equals, "go")
+	c.Check(err.Code, gc.Equals, ErrCode(0))
 	first = strings.SplitN(err.Context, "\n", 3)
-	t.Check(first[1], gc.Matches, "*TestWrap$")
-	t.Equal("Random error", err.Info["_err"])
-	t.Equal("Error: Random error", err.Message())
+	c.Check(first[1], gc.Matches, "*TestWrap$")
+	c.Check(err.Info["_err"], gc.Equals, "Random error")
+	c.Check(err.Message(), gc.Equals, "Error: Random error")
 	lines = strings.Split(err.Error(), "\n")
-	t.Equal("[go:0] Error: Random error", lines[0])
+	c.Check(lines[0], gc.Equals, "[go:0] Error: Random error")
 
 	err = Wrap(NewError(EMyError1))
-	t.Not(t.Nil(err))
-	t.Equal("ergo", err.Domain)
-	t.Equal(EMyError1, err.Code)
+	c.Check(err, gc.NotNil)
+	c.Check(err.Domain, gc.Equals, "ergo")
+	c.Check(err.Code, gc.Equals, EMyError1)
 	first = strings.SplitN(err.Context, "\n", 3)
-	t.Check(first[1], gc.Matches, "*TestWrap$")
-	t.Equal(errors[EMyError1], err.Message())
+	c.Check(first[1], gc.Matches, "*TestWrap$")
+	c.Check(err.Message(), gc.Equals, errors[EMyError1])
 	lines = strings.Split(err.Error(), "\n")
-	t.Equal("[ergo:1] My error 1", lines[0])
+	c.Check(lines[0], gc.Equals, "[ergo:1] My error 1")
 
 	err = Wrap(nil)
-	t.Nil(err)
+	c.Check(err, gc.IsNil)
 }
 
-func (t *TestSuite) TestNoDomain() {
+func (t *TestSuite) TestNoDomain(c *gc.C) {
 	err := New(0, "x", 1, "arg", "x")
-	t.Not(t.Nil(err))
-	t.Equal("x", err.Domain)
-	t.Equal(ErrCode(1), err.Code)
+	c.Check(err, gc.NotNil)
+	c.Check(err.Domain, gc.Equals, "x")
+	c.Check(err.Code, gc.Equals, ErrCode(1))
 	first := strings.SplitN(err.Context, "\n", 3)
-	t.Check(first[1], gc.Matches, "*TestNoDomain$")
+	c.Check(first[1], gc.Matches, "*TestNoDomain$")
 	const msg = "Domain missing: [x:1] map[arg:x]"
-	t.Equal(msg, err.Message())
+	c.Check(err.Message(), gc.Equals, msg)
 	lines := strings.Split(err.Error(), "\n")
-	t.Equal("[x:1] "+msg, lines[0])
+	c.Check(lines[0], gc.Equals, "[x:1] "+msg)
 }
 
-func (t *TestSuite) TestChain() {
+func (t *TestSuite) TestChain(c *gc.C) {
 	inner := NewError(EMyError0)
 	middle := Chain(inner, NewError(EMyError0))
 	outer := Chain(middle, NewError(EMyError1))
-	t.Not(t.Nil(outer))
-	t.Equal(inner, Cause(inner))
-	t.Equal(inner, Cause(middle))
-	t.Equal(inner, Cause(outer))
+	c.Check(outer, gc.NotNil)
+	c.Check(Cause(inner), gc.Equals, inner)
+	c.Check(Cause(middle), gc.Equals, inner)
+	c.Check(Cause(outer), gc.Equals, inner)
 	msg := outer.Error()
 	chains := strings.Split(msg, "\n\n")
 	lines0 := strings.Split(chains[0], "\n")
 	lines1 := strings.Split(chains[1], "\n")
 	lines2 := strings.Split(chains[2], "\n")
-	t.Equal("[ergo:0] My error 0", lines0[0])
-	t.Equal("[ergo:0] My error 0", lines1[0])
-	t.Equal("[ergo:1] My error 1", lines2[0])
+	c.Check(lines0[0], gc.Equals, "[ergo:0] My error 0")
+	c.Check(lines1[0], gc.Equals, "[ergo:0] My error 0")
+	c.Check(lines2[0], gc.Equals, "[ergo:1] My error 1")
 }
